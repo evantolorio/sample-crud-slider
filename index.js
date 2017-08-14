0c14e263fbeb596a8a9bc686ad1955d4262b1e0e
Evan Tolorio <evantolorio@gmail.com>
2017-08-14 15:48:51 +0800

    Setup initial routes

--- index.js
+++ index.js
@@ -8,30 +8,42 @@ mongoose.connect('mongodb://localhost:27017/test');
 // Parameters: model name, schema, collection name
 var Slider = mongoose.model('Slider', sliderSchema, 'sliders');
 
-var slider = new Slider({
-    backgroundImage: '/assets/images/community.jpg',
-    caption: {
-        title: 'Strong Community',
-        description: 'Build an interactive and solid community driven by purpose, and united in achieving common goals.',
-        actionButton: {
-            shouldUse: false,
-            text: '',
-            color: ''
-        },
-        background: {
-            color: 'blue',
-            opacity: '0.7'
-        }
-    },
-    order: 1
+// var slider = new Slider({
+//     backgroundImage: '/assets/images/community.jpg',
+//     caption: {
+//         title: 'Strong Community',
+//         description: 'Build an interactive and solid community driven by purpose, and united in achieving common goals.',
+//         actionButton: {
+//             shouldUse: false,
+//             text: '',
+//             color: ''
+//         },
+//         background: {
+//             color: 'blue',
+//             opacity: '0.7'
+//         }
+//     },
+//     order: 1
+// });
+//
+// slider.save(function(error){
+//     if (error) {
+//         console.log(error);
+//         process.exit(1);
+//     }
+//     else {
+//         console.log('sucess!');
+//     }
+// });
+
+var app = express();
+
+// Serve static files
+app.use(express.static('assets'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-slider.save(function(error){
-    if (error) {
-        console.log(error);
-        process.exit(1);
-    }
-    else {
-        console.log('sucess!');
-    }
-});
+app.listen(9090);
+console.log('Server listening on port 9090');
